3efea369ce234c07e6c2841ae4a50343c955b6d7
alberto <alberto.taddeucci@gmail.com>
2024-02-12 12:59:56 +0100

    ciclo for per inserimento in array

--- Snack2/script.js
+++ Snack2/script.js
@@ -6,8 +6,20 @@ prendendo alternativamente gli elementi da uno e dall’altro
 
 */
 
+const array1 = [1,2,3,4,5];
+
+const array2 = ["a","b","c","d","e"];
+
+const array3 = [];
 
 
+for(i=0;i<array1.length;i++){
+    array3.push(array1[i])
+    array3.push(array2[i])
+
+}
+
+console.log(array3)
 
 
 
